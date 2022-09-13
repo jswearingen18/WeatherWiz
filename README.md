8158515af2794c5c4902b49a293533c75eb53660
Jeremiah Swearingen <jswearingen18@gmail.com>
2022-09-12 22:17:28 -0500

    added weather and temp

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # WeatherWiz
+
+## Weather program 
+
+## Description
+Will tell you the weather for the current day as well as for the current week. Uses Modem.JS fdor time and date. Will allow you to search for other cities using the one map weather api 
+
+## Github URL link
+
+
+ ## Github Reposisitory link
+ https://github.com/jswearingen18/WeatherWiz
+
+ ## Badges
+
+ ## Visuals
+
+
+ ## Usage
+Tells you the weather forecast for current day and week
+
+ ## Roadmap
+ use fetch to retrieve api for weather not just coordinates
+
+ ## Contributing
+ Thanks to vanderbilt university and Trilogy Education for the knowledge to build this as well as starter code.
+
+ ## Authors and acknowledgement
+Built by Jeremiah Swearingen 
+
+## License
+Licensed through Trilogy Eductaion and Vanderbilt Univeristy 
--- asset/script.js
+++ asset/script.js
@@ -1,47 +1,28 @@
 var apiKey = "e2c0b0b0315d59bb35cf0a434cb83813";
 var searchHistory = [];
-var searchEl = document.querySelector("#searchForm");
-var City = document.querySelector("#searchInput").value;
+var searchEl = document.querySelector("#searchInput");
 var currentDay = document.querySelector("#today");
 var forecastEl = document.querySelector("#foreCast");
 var previousSearch = document.querySelector("#prevSearch");
 var apiUrl = "http://api.openweathermap.org/"
+var searchBtn = document.getElementById("myBtn");
+var temp = document.getElementById("temp");
+var weather = document.getElementById("weather");
+var icon = document.getElementById("icon");
 
-function fetchCoordinates(coordinates) {
-    var coordUrl = `${apiUrl}geo/1.0/direct?q=Chicago&limit=5&appid=${apiKey}`;
-    fetch(coordUrl)
-    
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        if (!data[0]) {
-            alert("Location not found");
-        } else {
-            //appendTosearchHistory(search);
-            //fetchWeather(data[0]);
-            var lat = data[0].lat;
-            var lon = data[0].lon;
-            var weatherUrl = `${apiUrl}data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
-            fetchWeather(weatherUrl);
+   function getWeather () {
+    const userInput = searchEl.value;
+    let weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=imperial&us&APPID=${apiKey}`;
+fetch(weatherApi)
+.then (function (data) {
+    return data.json(data);
+})
+.then (function (data) {
+    icon.innerHTML = data.weather[0].icon;
+    temp.innerHTML = "Temperature: " + data.main.temp;
+    console.log(data);
+    weather.innerHTML ="Weather: " + data.weather[0].description;
 
-        }
-        })
-        .catch(function (err) {
-            console.error(err);
-        });
-    }
-    fetchCoordinates();
-    function fetchWeather(weatherUrl) {
-        fetch(weatherUrl)
-        
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data[0].weather);
-        })
-        .catch(function (err) {
-        console.error(err);
-    });
-    }
+})
+}
+searchBtn.addEventListener("click",getWeather);
