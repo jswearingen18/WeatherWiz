532dd6155ae97856bca4c62f6c7050a4fdb0eeef
Jeremiah Swearingen <jswearingen18@gmail.com>
2022-09-12 22:22:50 -0500

    update with comments

--- asset/script.js
+++ asset/script.js
@@ -1,3 +1,4 @@
+// Variables used for the different elements
 var apiKey = "e2c0b0b0315d59bb35cf0a434cb83813";
 var searchHistory = [];
 var searchEl = document.querySelector("#searchInput");
@@ -9,14 +10,16 @@ var searchBtn = document.getElementById("myBtn");
 var temp = document.getElementById("temp");
 var weather = document.getElementById("weather");
 var icon = document.getElementById("icon");
-
+// Function to fetch data using openweather API
    function getWeather () {
     const userInput = searchEl.value;
     let weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=imperial&us&APPID=${apiKey}`;
 fetch(weatherApi)
+// Then statement to return data as json
 .then (function (data) {
     return data.json(data);
 })
+// Then statement to display data after you have searched for a city
 .then (function (data) {
     icon.innerHTML = data.weather[0].icon;
     temp.innerHTML = "Temperature: " + data.main.temp;
@@ -25,4 +28,5 @@ fetch(weatherApi)
 
 })
 }
+// Event listener to run the get weather function
 searchBtn.addEventListener("click",getWeather);
